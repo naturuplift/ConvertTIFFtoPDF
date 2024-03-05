127f6c834ec86c8b0d6db95f8891fe677d5cc073
Arnaldo <arnaldosepulveda@gmail.com>
2024-03-04 22:46:30 -0400

    added merging of pdf pages into one pdf file

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 const { PDFDocument } = require('pdf-lib');
 const sharp = require('sharp');
 
-// number of pages
-const pages = 1; // for File
-
 // main function to convert TIFF to PDF
-async function convertTiffToPdf() {
+async function convertTiffToPdf(pages) {
+  let filePaths = []; // for pdf files
+
   // convert each page
-  for (let index = 1; index < pages+1; index++) {
+  for (let index = 1; index <= pages; index++) {
 
     // Example usage
     const tiffPath = `./assets/File ${index}.tif`;
@@ -45,11 +44,48 @@ async function convertTiffToPdf() {
       // Write the PDF to a file
       fs.writeFileSync(pdfPath, pdfBytes);
 
+      console.log(pdfPath)
+
+      // Dynamically add the created PDF path to the filePaths array
+      filePaths.push(pdfPath);
+
       console.log('TIFF to PDF conversion completed successfully.');
     } catch (error) {
       console.error('Error converting TIFF to PDF:', error);
     }
-  } 
+  }
+
+  console.log(filePaths)
+
+  // Merge pages once all pages converted to individual pdf's
+  if(filePaths.length > 0) {
+    await mergePdfs(filePaths);
+    console.log('All TIFF files have been converted and merged into one PDF document.');
+  } else {
+    console.log('No TIFF files were converted.');
+  }
 }
 
-convertTiffToPdf();
+async function mergePdfs(paths) {
+  const mergedPdf = await PDFDocument.create();
+
+  for (const path of paths) {
+      // Load a PDF document from the file system
+      const pdfBytes = fs.readFileSync(path);
+      const pdfDoc = await PDFDocument.load(pdfBytes);
+
+      // Copy all pages from the loaded PDFs into the first one
+      const copiedPages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
+      copiedPages.forEach((page) => mergedPdf.addPage(page));
+  }
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const mergedPdfFile = await mergedPdf.save();
+
+  // Write the merged PDF to a file
+  fs.writeFileSync('./assets/merged.pdf', mergedPdfFile);
+
+  console.log('PDFs merged successfully!');
+}
+
+convertTiffToPdf(7);
